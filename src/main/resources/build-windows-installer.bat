30c49c2c909e5360c4f5a759da517c031801bd40
BaneleMlamleli <mlamlelibanele@yahoo.com>
2026-04-26 04:30:07 -0700

    Update build-windows-installer.bat with correct file paths

--- src/main/resources/build-windows-installer.bat
+++ src/main/resources/build-windows-installer.bat
@@ -1,7 +1,7 @@
-jpackage --input C:/Users/banelemlamleli/Documents/Programming/Java/BatchFileCreator \
+jpackage --input C:/Users/dev/Documents/BatchFileCreator \
     --name BatchFileCreator \
     --main-jar BatchFileCreator.jar \
     --app-version "1.0.1" \
     --main-class com.banelemlamleli.batchfilecreator.BatchFileCreator \
     --type exe \
-    --icon "C:/Users/banelemlamleli/Documents/Programming/Java/BatchFileCreator/src/main/resources/icons/paper_stack.ico"
+    --icon "C:/Users/dev/Documents/BatchFileCreator/src/main/resources/icons/paper_stack.ico"
